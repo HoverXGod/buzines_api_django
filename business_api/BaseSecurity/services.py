f10f1eb5c12e91dcb7cabbfb505ab033e471490d
HoverXGod <krasotkinnikitushka@yandex.ru>
2025-01-12 01:23:42 +0400

    Add 'InternalApiError' for default error answer

--- business_api/BaseSecurity/services.py
+++ business_api/BaseSecurity/services.py
@@ -6,6 +6,11 @@ class SecureResponse(Response):
     def __init__(self, request, data=None, status=201, headers=None, content_type=None):
 
         request.log.request_status = status
+
+        if status != 201 and status != 200:
+            if data == None:
+                data = 'ÍnteralApiError'
+
         return super().__init__(data=data, status=status, headers=headers, content_type=content_type)
 
     
