3c05e08f6c5c2556f55d444ec51fbb3c59e35873
HoverXGod <krasotkinnikitushka@yandex.ru>
2025-01-12 01:33:49 +0400

    HttpResponde status mini hendler

--- business_api/BaseSecurity/services.py
+++ business_api/BaseSecurity/services.py
@@ -1,15 +1,22 @@
 from rest_framework.response import Response
-
+from django.contrib.auth.models import AnonymousUser
 
 class SecureResponse(Response):
     
     def __init__(self, request, data=None, status=201, headers=None, content_type=None):
 
-        request.log.request_status = status
-
         if status != 201 and status != 200:
             if data == None:
                 data = 'ÍnteralApiError'
+            elif status == 400:
+                data = {"BadRequest": data}
+            elif status == 403:
+                data = {"PermissionError": data}
+            elif status == 403 and not request.user.is_authenticated:
+                data = 'User is not autorized'
+                status = 401
+
+        request.log.request_status = status
 
         return super().__init__(data=data, status=status, headers=headers, content_type=content_type)
 
