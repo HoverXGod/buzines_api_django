07a648ff39dec2a449c4f0b1db601fecb698ef9b
HoverXGod <krasotkinnikitushka@yandex.ru>
2025-01-13 00:52:31 +0400

    a small add and start reolized Produc applcation

--- business_api/BaseSecurity/services.py
+++ business_api/BaseSecurity/services.py
@@ -1,5 +1,4 @@
 from rest_framework.response import Response
-from django.contrib.auth.models import AnonymousUser
 
 class SecureResponse(Response):
     
@@ -19,5 +18,7 @@ class SecureResponse(Response):
         request.log.request_status = status
 
         return super().__init__(data=data, status=status, headers=headers, content_type=content_type)
+
+class SessionManager: pass
 
     
--- business_api/Product/models.py
+++ business_api/Product/models.py
@@ -1,3 +1,62 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    """Модель категори к которой относятся товары"""
+
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    image = models.CharField(max_length=256, default='')
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = 'Категория'  # Имя модели в единственном числе
+        verbose_name_plural = 'Категории'  # Имя модели во множественном числе
+
+    @property
+    def active(self): return self.active
+
+    @active.setter
+    def active(self, value): 
+        self.active = value
+        self.save()
+
+    @staticmethod
+    def create_category(): pass
+
+    def update_category(self): pass
+
+    def delete_category(self): pass
+
+class Product(models.model):
+    """Модель товара относящегося к категории"""
+
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    price = models.FloatField()
+    weight = models.FloatField(default=0)
+    have_weight = models.BooleanField(default=False)
+    image = models.CharField(max_length=256, default='')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = 'Продукт'  # Имя модели в единственном числе
+        verbose_name_plural = 'Продукты'  # Имя модели во множественном числе
+
+    @property
+    def active(self): return self.active
+
+    @active.setter
+    def active(self, value): 
+        self.active = value
+        self.save()
+    
+    @staticmethod
+    def create_product(): pass
+
+    def update_product(self): pass
+
+    @staticmethod
+    def get_products(category_name): pass
+
+    def delete_product(self): pass
